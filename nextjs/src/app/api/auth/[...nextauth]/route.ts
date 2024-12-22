53f790c83dfd8b835241a353979c176ab7a5e7cb
kishyda <marcuspyon@gmail.com>
2024-12-22 10:43:13 +0900

    database set up, progress towards chat functionality

---
+++ nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+if (!googleClientId) {
+    throw new Error("GOOGLE_CLIENT_ID is not set");
+}
+
+if (!googleClientSecret) {
+    throw new Error("GOOGLE_CLIENT_SECRET is not set");
+}
+
+const handler = NextAuth({
+    providers: [
+        Google({
+            clientId: googleClientId,
+            clientSecret: googleClientSecret
+        })
+    ]
+});
+
+export { handler as GET, handler as POST };
