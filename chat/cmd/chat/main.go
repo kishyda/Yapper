68251386b20967fbe54d397c151b50040946aa8e
kishyda <marcuspyon@gmail.com>
2025-02-15 17:06:12 -0800

    updated chat, now works

--- chat/cmd/chat/main.go
+++ chat/cmd/chat/main.go
@@ -14,6 +14,40 @@ import (
 	"github.com/rs/cors"
 )
 
+type jsonOject struct {
+	ChatID string `json:"chatID"`
+	UserID string `json:"userID"`
+}
+
+type Message struct {
+	AccountID   string `json:"accountID"`
+	AccountName string `json:"accountName"`
+	ChatID      string `json:"chatID"`
+	ChatName    string `json:"chatName"`
+	Message     string `json:"message"`
+	Time        string `json:"time"`
+	Close       bool   `json:"close"`
+}
+
+type Chat struct {
+	ChatName string `json:"chatName"`
+	ChatID   string `json:"chatID"`
+	Users    string `json:"users"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+type Success struct {
+	Message string `json:"message"`
+}
+
+type Connection struct {
+	Conn  *websocket.Conn
+	mutex *sync.Mutex
+}
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
--- chat/cmd/chat/structs.go
+++ chat/cmd/chat/structs.go
@@ -1,41 +1,35 @@
 package main
 
-import (
-	"sync"
+// type jsonOject struct {
+// 	ChatID string `json:"chatID"`
+// 	UserID string `json:"userID"`
+// }
 
-	"github.com/gorilla/websocket"
-)
+// type Message struct {
+// 	AccountID   string `json:"accountID"`
+// 	AccountName string `json:"accountName"`
+// 	ChatID      string `json:"chatID"`
+// 	ChatName    string `json:"chatName"`
+// 	Message     string `json:"message"`
+// 	Time        string `json:"time"`
+// 	Close       bool   `json:"close"`
+// }
 
-type jsonOject struct {
-	ChatID string `json:"chatID"`
-	UserID string `json:"userID"`
-}
+// type Chat struct {
+// 	ChatName string `json:"chatName"`
+// 	ChatID   string `json:"chatID"`
+// 	Users    string `json:"users"`
+// }
 
-type Message struct {
-	AccountID   string `json:"accountID"`
-	AccountName string `json:"accountName"`
-	ChatID      string `json:"chatID"`
-	ChatName    string `json:"chatName"`
-	Message     string `json:"message"`
-	Time        string `json:"time"`
-	Close       bool   `json:"close"`
-}
+// type Error struct {
+// 	Message string `json:"message"`
+// }
 
-type Chat struct {
-	ChatName string `json:"chatName"`
-	ChatID   string `json:"chatID"`
-	Users    string `json:"users"`
-}
+// type Success struct {
+// 	Message string `json:"message"`
+// }
 
-type Error struct {
-	Message string `json:"message"`
-}
-
-type Success struct {
-	Message string `json:"message"`
-}
-
-type Connection struct {
-	Conn  *websocket.Conn
-	mutex *sync.Mutex
-}
+// type Connection struct {
+// 	Conn  *websocket.Conn
+// 	mutex *sync.Mutex
+// }
